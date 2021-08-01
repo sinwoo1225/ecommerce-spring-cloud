ec2b5c8ecf0ce3f0306e51c2f81007cf0659eff1
shinwoo <sinwoo1225@gmail.com>
2021-08-01 13:28:07 +0900

    Spring Cloud Gateway 추가(netflix zuul 대용)

--- first-service/src/main/java/siru/firstservice/FirstServiceController.java
+++ first-service/src/main/java/siru/firstservice/FirstServiceController.java
@@ -1,16 +1,26 @@
 package siru.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+
+      log.info("Header[{}]", header);
+      return "Hello World Request First Service";
     }
 
 }
--- second-service/src/main/java/siru/secondservice/SecondServiceController.java
+++ second-service/src/main/java/siru/secondservice/SecondServiceController.java
@@ -1,16 +1,26 @@
 package siru.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+
+        log.info("Header[{}]", header);
+        return "Hello World Request Second Service";
     }
 
 }
