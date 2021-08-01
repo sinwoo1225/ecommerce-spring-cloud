34bd475d14f09b7fd90c5ba2eca6adcc725aec94
shinwoo <sinwoo1225@gmail.com>
2021-08-01 12:09:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ecommerce Application with Spring Cloud
---
+++ second-service/src/test/java/siru/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package siru.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
